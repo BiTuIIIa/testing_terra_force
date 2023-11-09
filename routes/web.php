7438c21925b3564baf906b59894a7713fb48414e
BiTuIIIa <vikhoriainov@gmail.com>
2023-11-09 18:56:44 +0200

    add routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\RegistrationController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\XhrPostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +20,24 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+})->name('welcome');
+
+Route::group(['prefix' => 'auth', 'as' => 'auth.'], function() {
+    Route::get('/logout', [LogoutController::class, 'index'])->name('logout');
+    Route::get('/login', [LoginController::class, 'index'])->name('login');
+    Route::post('/login', [LoginController::class, 'authenticate'])->name('authenticate');
+    Route::get('/registration', [RegistrationController::class, 'index'])->name('index.registration');
+    Route::post('/registration',[RegistrationController::class, 'registration'])->name('registration');
+});
+
+
+Route::middleware(['auth'])->group(function () {
+    Route::name('user.')->group(function () {
+        Route::view('home', 'home')->name('home');
+    });
+    Route::resource('posts', PostController::class);
+
+    Route::group(['prefix' => 'xhr'], function() {
+        Route::get('/posts/', [XhrPostController::class, 'index']);
+    });
 });
